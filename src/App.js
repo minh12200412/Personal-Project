2b21fe7ab222629cf1918834c307e6802ce07d03
minh12200412~ <minhvuive122004@gmail.com>
2024-07-06 08:59:07 +0700

    NavBar-Project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.scss";
+import Header from "./Components/Header/Header";
 
 function App() {
-  return <div className=""></div>;
+  return (
+    <div className="App-container">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
