a36863fd79ff55525b8be6d9068e8fd02d8241ce
minh12200412~ <minhvuive122004@gmail.com>
2024-07-07 06:33:30 +0700

    RouterSS1

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,24 +2,46 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">Đuc Minh Code</Navbar.Brand>
+        <Navbar.Brand href="">Duc Minh Code</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Link className="nav-link" to="/">
+              Home
+            </Link>
+            <Link className="nav-link" to="/users">
+              User
+            </Link>
+            <Link className="nav-link" to="/admins">
+              Admin
+            </Link>
+          </Nav>
+          <Nav className="me-auto">
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
           </Nav>
           <Nav className="">
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+            <NavDropdown title="Settings" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Log in</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">Log out</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">Profile</NavDropdown.Item>
               <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Help</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
--- src/index.js
+++ src/index.js
@@ -2,14 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { Provider } from "react-redux";
+// import store from "./app/store";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import User from "./Components/User/User";
+import Admin from "./Components/Admin/Admin";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+  // <Provider store={store}>
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="users" element={<User />} />
+        <Route path="admins" element={<Admin />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
+  // </Provider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
