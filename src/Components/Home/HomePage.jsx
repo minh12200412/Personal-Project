62a427a148ea18ed1d50917a1c85f5077169a3d2
minh12200412~ <minhvuive122004@gmail.com>
2024-07-09 23:07:31 +0700

    RouterSS3

--- src/Components/Home/HomePage.jsx
+++ src/Components/Home/HomePage.jsx
@@ -1,6 +1,13 @@
 import React from "react";
+import VideoHomePage from "../../Assets/Video/video-homepage.mp4";
 
 const HomePage = (props) => {
-  return <div className="homepage-container">HomePage</div>;
+  return (
+    <div className="homepage-container">
+      <video autoPlay muted loop>
+        <source src={VideoHomePage} type="video/mp4" />
+      </video>
+    </div>
+  );
 };
 export default HomePage;
