744b845fd4521e546af561775fd09361ff83b36a
minh12200412~ <minhvuive122004@gmail.com>
2024-07-12 11:02:50 +0700

    RouterSideBar

--- src/Components/Admin/Admin.jsx
+++ src/Components/Admin/Admin.jsx
@@ -1,6 +1,25 @@
 import React from "react";
+import SideBal from "./SideBal";
+import "./Admin.scss";
+import { FaBars } from "react-icons/fa";
+import { useState } from "react";
 
 const Admin = (props) => {
-  return <div>Admin</div>;
+  const [collapsed, setCollapsed] = useState(false);
+  return (
+    <div className="admin-container">
+      <div className="admin-sidebar">
+        <SideBal collapsed={collapsed} />
+      </div>
+      <div className="admin-content">
+        <FaBars
+          onClick={() => {
+            setCollapsed(!collapsed);
+          }}
+        />
+        <p>I am Duc Minh </p>
+      </div>
+    </div>
+  );
 };
 export default Admin;
