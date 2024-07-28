79c2dd40daeb357960d162bdd5dd506a8d7da330
minh12200412~ <minhvuive122004@gmail.com>
2024-07-28 15:58:04 +0700

    StateModalsUser

--- src/Components/Admin/Contents/ModalsUser.jsx
+++ src/Components/Admin/Contents/ModalsUser.jsx
@@ -9,8 +9,19 @@ function Example() {
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [role, setRole] = useState("USER");
+  const [image, setImage] = useState("");
+  const [previewImg, setPreviewImg] = useState("");
   const handleUploadFile = (even) => {
-    console.log("up load");
+    if (even.target && even.target.files && even.target.files[0]) {
+      setImage(even.target.files[0]);
+      setPreviewImg(URL.createObjectURL(even.target.files[0]));
+    } else {
+      setPreviewImg("");
+    }
   };
   return (
     <>
@@ -34,13 +45,27 @@ function Example() {
               <label for="inputEmail4" className="form-label">
                 Email
               </label>
-              <input type="email" className="form-control" />
+              <input
+                type="email"
+                className="form-control"
+                value={email}
+                onChange={(even) => {
+                  setEmail(even.target.value);
+                }}
+              />
             </div>
             <div className="col-md-6">
               <label for="inputPassword4" className="form-label">
                 Password
               </label>
-              <input type="password" className="form-control" />
+              <input
+                type="password"
+                className="form-control"
+                value={password}
+                onChange={(even) => {
+                  setPassword(even.target.value);
+                }}
+              />
             </div>
             <div className="col-md-6">
               <label for="inputUserName" className="form-label">
@@ -48,17 +73,27 @@ function Example() {
               </label>
               <div className="input-group">
                 <div className="input-group-text">@</div>
-                <input type="text" className="form-control" />
+                <input
+                  type="text"
+                  className="form-control"
+                  value={username}
+                  onChange={(even) => {
+                    setUsername(even.target.value);
+                  }}
+                />
               </div>
             </div>
             <div className="col-md-4">
               <label for="inputState" className="form-label">
                 Role
               </label>
-              <select className="form-select">
-                <option selected value="USER">
-                  User
-                </option>
+              <select
+                className="form-select"
+                onChange={(even) => {
+                  setRole(even.target.value);
+                }}
+              >
+                <option value="USER">User</option>
                 <option value="ADMIN">Admin</option>
               </select>
             </div>
@@ -80,11 +115,11 @@ function Example() {
               />
             </div>
             <div className="col-md-12 img-preview">
-              {/* <span>Preview Image</span> */}
-              <img
-                src="https://phunugioi.com/wp-content/uploads/2020/03/hinh-nen-may-tinh-4k-de-thuong-scaled.jpg"
-                alt=""
-              />
+              {previewImg ? (
+                <img src={previewImg} alt="" />
+              ) : (
+                <span>Preview Image</span>
+              )}
             </div>
           </form>
         </Modal.Body>
