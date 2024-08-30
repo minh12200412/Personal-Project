1865b263bdbb7f1c9b8f72669b3dcd5472ab8a98
minh12200412~ <minhvuive122004@gmail.com>
2024-08-30 21:57:44 +0700

    Processing Data when Selected Answer

--- src/Components/User/DetailQuestion.jsx
+++ src/Components/User/DetailQuestion.jsx
@@ -4,12 +4,19 @@ const DetailQuestion = (prop) => {
   if (_.isEmpty(data)) {
     return <></>;
   }
+  const handleHandAnswers = (event, questionID, answersID) => {
+    console.log("Test :", event.target.checked);
+    // console.log(questionID + answersID);
+    prop.handleCheckBox(questionID, answersID);
+  };
   return (
     <>
-      {data.image && (
+      {data.image ? (
         <div className="body-q">
           <img src={`data:image/png;base64,${data.image}`} alt="" />
         </div>
+      ) : (
+        <div className="body-q"></div>
       )}
       <div className="question">
         Question{index}: {data.questionDescription}
@@ -19,16 +26,17 @@ const DetailQuestion = (prop) => {
           data.answers.length &&
           data.answers.map((item, index) => {
             return (
-              <div class="form-check">
+              <div key={`answer${index}`} className="form-check">
                 <input
-                  class="form-check-input"
+                  className="form-check-input"
                   type="checkbox"
                   value=""
-                  id="flexCheckDefault"
+                  checked={item.isSelected}
+                  onChange={(event) => {
+                    handleHandAnswers(event, data.questionID, item.id);
+                  }}
                 />
-                <label class="form-check-label" for="flexCheckDefault">
-                  {item.description}
-                </label>
+                <label className="form-check-label">{item.description}</label>
               </div>
               //   <p key={`answers-${index}`} className="a-child">
               //     A-{item.description}
--- src/Components/User/DetailQuiz.jsx
+++ src/Components/User/DetailQuiz.jsx
@@ -17,7 +17,7 @@ const DetailQuiz = (props) => {
     }
   }, [quizId]);
   const location = useLocation();
-  console.log("location", location);
+  // console.log("location: ", location);
   const getDataQuiz = async () => {
     let res = await getQuizData(quizId);
     if (res && res.EC === 0) {
@@ -35,13 +35,14 @@ const DetailQuiz = (props) => {
               questionDescription = item.description;
               image = item.image;
             }
+            item.answers.isSelected = false;
             answers.push(item.answers);
           });
           return { questionID: key, answers, questionDescription, image };
         })
         .value();
       setDataQuiz(data);
-      console.log(data);
+      console.log("dataQuiz: ", data);
     }
   };
   const handlePrev = () => {
@@ -49,7 +50,29 @@ const DetailQuiz = (props) => {
     setIndex(index - 1);
   };
   const handleNext = () => {
-    if (dataQuiz && dataQuiz.length > index + 1) setIndex(index + 1);
+    if (dataQuiz && dataQuiz.length > index + 1) {
+      setIndex(index + 1);
+    }
+  };
+  const handleCheckBox = (questionID, answersID) => {
+    let dataQuizClone = _.cloneDeep(dataQuiz);
+    let question = dataQuizClone.find(
+      (item) => +item.questionID === +questionID
+    );
+    if (question && question.answers) {
+      question.answers = question.answers.map((item) => {
+        if (+item.id === +answersID) {
+          item.isSelected = !item.isSelected;
+        }
+        return item;
+      });
+    }
+    let index = dataQuizClone.findIndex(
+      (item) => +item.questionID === +questionID
+    );
+    if (index > -1) {
+      setDataQuiz(dataQuizClone);
+    }
   };
   return (
     <div className="detail-quiz-container">
@@ -60,7 +83,10 @@ const DetailQuiz = (props) => {
         </div>
         <hr />
         <div className="content-q">
-          <DetailQuestion data={dataQuiz[index]} />
+          <DetailQuestion
+            data={dataQuiz[index]}
+            handleCheckBox={handleCheckBox}
+          />
         </div>
         <div className="footer">
           <button className="btn btn-primary" onClick={() => handlePrev()}>
@@ -68,6 +94,9 @@ const DetailQuiz = (props) => {
           </button>
           <button className="btn btn-primary" onClick={() => handleNext()}>
             Next
+          </button>
+          <button className="btn btn-warning" onClick={() => handleNext()}>
+            Finish
           </button>
         </div>
       </div>
